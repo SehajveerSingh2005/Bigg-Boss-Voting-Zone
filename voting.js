887e375eb78588a4bb0e1b11f318de860889359f
Sehajveer Singh <156107192+SehajveerSingh2005@users.noreply.github.com>
2024-03-15 21:59:56 +0530

    Update voting.js

--- voting.js
+++ voting.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js"
 import { doc, setDoc,updateDoc,increment} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js"
+import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDXIajxTK_g31he872G4xnM-nlZ9cmfz6k",
@@ -16,10 +18,27 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 // Get reference to individual divs
 const cards = document.querySelectorAll('.card');
 let selectedcard = null;
+const loadingScreen = document.getElementById('loading-screen');
+loadingScreen.style.display = 'none';
+
+onAuthStateChanged(auth,(user)=>{
+
+  const signoutbtn = document.getElementById( 'signoutbtn' );
+  loadingScreen.style.display = 'block';
+
+  if (user) {
+      loadingScreen.style.display = 'none';
+      signoutbtn.style.display='block';
+  }
+  else{
+      window.location.href = 'signup.html';
+  }
+})
 
 // Add click event listener to each individual to handle selection
 cards.forEach((card) => {
@@ -33,12 +52,25 @@ cards.forEach((card) => {
 });
 
 const votebtn = document.getElementById("votebtn")
+const voteddiv = document.getElementById('votedmsg')
 
-document.getElementById("votebtn").addEventListener("click",castvote,{once:true});
-const disableTarget = () => {
-  votebtn.classList.add("disable")
+function thanksforvote(){
+  voteddiv.classList.add('msg')
+
+  setTimeout(function(){
+    voteddiv.classList.remove('msg');
+  },5000);
 }
-document.getElementById("votebtn").addEventListener("click", disableTarget);
+
+function disablebtn(){
+  votebtn.classList.add('disable')
+
+  setTimeout(function(){
+    votebtn.classList.remove('disable');
+  },5000);
+}
+
+votebtn.addEventListener("click",castvote);
 
 function castvote() {
   if (!selectedcard) {
@@ -52,4 +84,21 @@ function castvote() {
   updateDoc(votecountRef,{
     count: increment(1)
   })
+  thanksforvote();
+  disablebtn();
 }
+
+const signOutUser = () => {
+  auth.signOut().then(() => {
+      // Sign-out successful.
+      console.log('User signed out successfully');
+      // You can redirect the user to another page or perform any other actions after sign-out
+  }).catch((error) => {
+      // An error happened.
+      console.error('Sign-out error:', error);
+  });
+};
+
+const signoutbtn = document.getElementById( 'signoutbtn' );
+
+signoutbtn.addEventListener( 'click', signOutUser, false ) ;
